30d87b8097fa0dec3a3a442945945844de3e22e2
Jackie-Qin <jackieqin221@gmail.com>
2020-11-18 18:42:53 -0800

    modified test

--- unit_test.cpp
+++ unit_test.cpp
@@ -7,6 +7,7 @@
 #include "add_test.hpp"
 #include "sub_test.hpp"
 #include "pow_test.hpp"
+#include "VectorContainer_test.hpp"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
